75a0358aa3d66025bd26e560d91d227341c3c454
aldiyusronazhar <aldiyusronzhar@gmail.com>
2025-05-27 19:01:31 +0700

    reset

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,49 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// import Image from "next/image";
+import styles from "./page.module.scss";
+import Header from "@/components/header/Header";
+import About from "@/components/about/About";
+// import VerticalTag from "@/components/ui/verticaltag/VerticalTag";
+import Projects from "@/components/projects/Projects";
+import { projects } from "@/components/projects/projectsData";
+
+
+import Sez from "@/components/ui/sez/Sez";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    return (
+        <>
+            {/* <VerticalTag/> */}
+            <div className={styles.page__wrapper}>
+                <Header />
+                <Sez
+                    sez1="001"
+                    sez2="(About)"
+                    sez3="Scroll More ___"
+                    config={{
+                        sez1: { shuffle: false },
+                        sez2: { shuffle: false },
+                        sez3: { shuffle: true, duration: 3000, interval: 4000, loop: true }
+                    }}
+                />
+                <About />
+                <Sez
+                    sez1="002"
+                    sez2="(Projects)"
+                    sez3="Scroll More ___"
+                    config={{
+                        sez1: { shuffle: false },
+                        sez2: { shuffle: false },
+                        sez3: { shuffle: true, duration: 3000, interval: 4000, loop: true }
+                    }}
+                />
+            <div className={styles.page__projects}>
+                {projects.map((project) => (
+                    <Projects key={project.id} project={project} />
+                ))}
+            </div>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+
+            </div>
+        </>
+    );
 }
